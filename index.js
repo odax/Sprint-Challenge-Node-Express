fcfda66db45edb7a5966c847984773080750106e
odax <nr10@albion.edu>
2018-09-06 19:25:38 -0400

    implemented basic functionality on port 5000

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// const projectRoutes = require('./modules/projects');
+// const actionRoutes = require('./modules/actions');
+
+const server = express();
+
+// server.use('/projects', projectRoutes);
+// server.use('/actions', actionRoutes);
+
+server.use('/', (req, res) => {
+    res.send('API up and running!')
+})
+
+server.listen(5000, () => console.log('server is active and running on port 5000'));
