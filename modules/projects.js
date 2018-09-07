f8b2945282998750a44e372ecfd213ddc7a7d745
odax <nr10@albion.edu>
2018-09-07 14:33:32 -0400

    completed delete hanlder for projects

--- modules/projects.js
+++ modules/projects.js
@@ -27,23 +27,37 @@ router.get('/:id', (req, res) => {
 //add
 router.post('/', (req, res) => {
     const {name, description, id} = req.body;
-    //   if (!newProject.name || !newProject.description) {
-    //       res.status(400).json({message: 'needs title and content to post'})
-    //   } else {
-    //       if ((newProject.name.length <= 128)&&(newProject.description.length <= 128)) {
+      if (!name || !description) {
+          res.status(400).json({message: 'needs title and content to post'})
+      } else {
+          if ((name.length <= 128)&&(description.length <= 128)) {
               PH.insert({name, description, id})
               .then(project => {
-                  res.status(201).json({message: 'successful post'})
+                  res.status(201).json({message: 'successful post', project})
               })
               .catch(error => {
                   res.status(500).json({error: `Error: ${error}`})
               })
-    //       } else {
-    //           res.status(400).json({message: 'name and description need to be 128 characters or below'})
-    //       }
-    //   }
+          } else {
+              res.status(400).json({message: 'name and description need to be 128 characters or below'})
+          }
+      }
 })
 //remove
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    let projectToDelete;
+    PH.get(id)
+    .then(foundProject => {
+        project = {...foundProject};
+    })
+    .catch(err => {
+        res.status(404).json({error: `Error: ${error}`})
+    })
+    PH.remove(id).then(response => {
+        res.status(200).json({message: `successfully deleted ${response} projects: ${projectToDelete}`})
+    })
+})
 
 //update
 
